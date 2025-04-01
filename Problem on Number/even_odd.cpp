e0ba6f3202b47c81bff00babbffc2df989ad8524
legendcyber28 <prashantkatheriya9970@gmail.com>
2025-04-01 21:30:51 +0530

    Updated existing C++ file

--- Problem on Number/even_odd.cpp
+++ Problem on Number/even_odd.cpp
@@ -1,18 +1,26 @@
-#include<bits/stdc++.h>
+/*Example 1:
+Input: n=5
+Output: odd
+Explanation: 5 is not divisible by 2.
+
+Example 2:
+Input: n=6
+Output: even
+Explanation: 6 is divisible by 2.*/
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
-    if(n%2==0)
+    cin >> n;
+    if (n % 2 == 0)
     {
-        cout<<"Even_no";
-        
+        cout << "Even_no";
     }
     else
-        cout<<"Odd_no";
-    
-    return 0;      
-    
+        cout << "Odd_no";
+
+    return 0;
 }
