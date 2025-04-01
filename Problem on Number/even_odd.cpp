2c18427741616cc66f418022dc298fc8d48d994a
legendcyber28 <prashantkatheriya9970@gmail.com>
2025-04-01 21:22:07 +0530

    Added C++ question

---
+++ Problem on Number/even_odd.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Even_no";
+        
+    }
+    else
+        cout<<"Odd_no";
+    
+    return 0;      
+    
+}
